eb3449e38f889b272f868cdf9c7fa7d9dcc57f5b
Moe-Baker <moe4baker@gmail.com>
2024-10-17 18:17:31 +0300

    Initial Commit

---
+++ Commands/Create Hardlinks.bat
@@ -0,0 +1,48 @@
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+	
+Rem -----------------------------------------------------------------------------------
+
+cd "%~dp0"
+cd ..
+
+rmdir "%cd%\Wsla-Core\Shared-Global\Source"
+mklink /D "%cd%\Wsla-Core\Shared-Global\Source" "%cd%\Wsla-Shared"
+echo:
+
+rmdir "%cd%\Wsla-Unity\Assets\Systems\Wsla\Shared"
+mklink /D "%cd%\Wsla-Unity\Assets\Systems\Wsla\Shared" "%cd%\Wsla-Shared"
+echo:
+
+rmdir "%cd%\Wsla-Unity\Assets\Systems\Wsla\Shared"
+mklink /D "%cd%\Wsla-Unity\Assets\Systems\Wsla\Shared" "%cd%\Wsla-Shared"
+echo:
+
+Pause
---
+++ Wsla-Core/Coordinator-Server/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Wsla.Server.Coordinator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
