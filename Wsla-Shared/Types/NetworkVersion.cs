090c945d5e18892984dfea5e682a99a171fb7ef2
Moe-Baker <moe4baker@gmail.com>
2025-11-05 11:39:15 +0300

    Improve Network Version Parsing Logic

--- Wsla-Shared/Types/NetworkVersion.cs
+++ Wsla-Shared/Types/NetworkVersion.cs
@@ -21,6 +21,10 @@ namespace Wsla
         /// </summary>
         int Binary => (Major << 16) | (Minor << 8) | (Patch);
 
+        public const char SplitCharacter = '.';
+        public const int MaxOctetCharacterLength = 3;
+        public const int MaxVersionCharacterLength = (MaxOctetCharacterLength * 3) + (2);
+
         public override bool Equals(object obj)
         {
             if (obj is NetworkVersion other)
@@ -33,6 +37,7 @@ namespace Wsla
             return (Major == other.Major) && (Minor == other.Minor) && (Patch == other.Patch);
         }
 
+        public override string ToString() => $"{Major}{SplitCharacter}{Minor}{SplitCharacter}{Patch}";
         public override int GetHashCode() => Binary;
 
         public int CompareTo(NetworkVersion other) => Numerical.CompareTo(other.Numerical);
@@ -52,35 +57,50 @@ namespace Wsla
 
         public static bool TryParse(string text, out NetworkVersion version)
         {
-            var parts = text.Split('.');
-
-            if (TryParseSegment(parts, 0, out var major) is false)
+            var span = text.AsSpan();
+            return TryParse(span, out version);
+        }
+        public static bool TryParse(ReadOnlySpan<char> characters, out NetworkVersion version)
+        {
+            if (TrySplit(ref characters, out var major) is false)
             {
                 version = default;
                 return false;
             }
-
-            if (TryParseSegment(parts, 1, out var minor) is false)
+            if (TrySplit(ref characters, out var minor) is false)
             {
                 version = default;
                 return false;
             }
-
-            if (TryParseSegment(parts, 2, out var patch) is false)
+            if (TrySplit(ref characters, out var patch) is false)
             {
                 version = default;
                 return false;
             }
-
-            static bool TryParseSegment(string[] parts, int index, out byte destination)
+            static bool TrySplit(ref ReadOnlySpan<char> characters, out byte number)
             {
-                if (index >= parts.Length)
+                var index = characters.IndexOf(SplitCharacter);
+
+                ReadOnlySpan<char> octet;
+
+                if (index < 0)
                 {
-                    destination = 0;
-                    return false;
+                    octet = characters;
+                    characters = default;
+                }
+                else
+                {
+                    octet = characters.Slice(0, index);
+                    characters = characters.Slice(index + 1);
                 }
 
-                return byte.TryParse(parts[index], out destination);
+                if (octet.Length == 0)
+                {
+                    number = 0;
+                    return true;
+                }
+
+                return byte.TryParse(octet, out number);
             }
 
             version = new NetworkVersion(major, minor, patch);
