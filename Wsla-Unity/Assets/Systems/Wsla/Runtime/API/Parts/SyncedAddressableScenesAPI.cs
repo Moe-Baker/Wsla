a7771b3f4ef6eed28c7b13f04b63f0e94f6220bb
Moe-Baker <moe4baker@gmail.com>
2025-11-13 08:23:08 +0300

    Improve Addressable Network Scene Reference Implementation/Usages

--- Wsla-Unity/Assets/Systems/Wsla/Runtime/API/Parts/SyncedAddressableScenesAPI.cs
+++ Wsla-Unity/Assets/Systems/Wsla/Runtime/API/Parts/SyncedAddressableScenesAPI.cs
@@ -30,6 +30,28 @@ namespace Wsla.Unity
             return keys;
         }
 
+        public NetworkSceneID GetID(AddressableNetworkSceneReference reference)
+        {
+            if (TryGetID(reference, out var id) is false)
+                throw new ArgumentException($"Scene ({reference}) Not Registered as Synced Addresable Scene");
+
+            return id;
+        }
+        public bool TryGetID(AddressableNetworkSceneReference reference, out NetworkSceneID id)
+        {
+            for (byte i = 0; i < Scenes.Length; i++)
+            {
+                if (reference == Scenes[i])
+                {
+                    id = new(i, NetworkSceneSource.Addressable);
+                    return true;
+                }
+            }
+
+            id = default;
+            return false;
+        }
+
         public bool TryGetReference(NetworkSceneID id, out AssetReference reference)
         {
             if (Scenes.IsValidIndex(id.Index) is false)
@@ -68,13 +90,25 @@ namespace Wsla.Unity
     //Provided by JamesFrowenDev
     //https://discussions.unity.com/t/something-like-assetreferencet-sceneasset/773034/9
     [Serializable]
-    public class AddressableNetworkSceneReference : AssetReference
+    public class AddressableNetworkSceneReference : AssetReference, IEquatable<AddressableNetworkSceneReference>
     {
         /// <summary>
         /// Construct a new AssetReference object.
         /// </summary>
         /// <param name="guid">The guid of the asset.</param>
         public AddressableNetworkSceneReference(string guid) : base(guid) { }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is AddressableNetworkSceneReference other)
+                return Equals(other);
+
+            return false;
+        }
+        public bool Equals(AddressableNetworkSceneReference other)
+        {
+            return (this.AssetGUID == other.AssetGUID);
+        }
 
         /// <inheritdoc/>
         public override bool ValidateAsset(UnityEngine.Object obj)
@@ -87,7 +121,6 @@ namespace Wsla.Unity
 #endif
 
         }
-
         /// <inheritdoc/>
         public override bool ValidateAsset(string path)
         {
@@ -105,6 +138,9 @@ namespace Wsla.Unity
         /// </summary>
         public new UnityEditor.SceneAsset editorAsset => (UnityEditor.SceneAsset)base.editorAsset;
 #endif
+
+        public static bool operator ==(AddressableNetworkSceneReference a, AddressableNetworkSceneReference b) => a.Equals(b);
+        public static bool operator !=(AddressableNetworkSceneReference a, AddressableNetworkSceneReference b) => !a.Equals(b);
     }
 }
 #endif
